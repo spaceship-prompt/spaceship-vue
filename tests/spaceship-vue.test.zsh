bdc75cae4a1a45b92ee6908a80071edac3fb9d9f
Denys Dovhan <denysdovhan@gmail.com>
2023-01-04 20:05:00 +0200

    test: add tests for vue section

--- tests/spaceship-vue.test.zsh
+++ tests/spaceship-vue.test.zsh
@@ -8,10 +8,12 @@ SHUNIT_PARENT=$0
 # Use system Spaceship or fallback to Spaceship Docker on CI
 typeset -g SPACESHIP_ROOT="${SPACESHIP_ROOT:=/spaceship}"
 
-# Mocked tool CLI
-mocked_version="v1.0.0-mocked"
-foobar() {
-  echo "$mocked_version"
+setupVue() {
+  mkdir -p $SHUNIT_TMPDIR/node_modules/vue
+  cp $CWD/package.vue.json $SHUNIT_TMPDIR/node_modules/vue/package.json
+
+  # Enter the test directory
+  cd $SHUNIT_TMPDIR
 }
 
 # ------------------------------------------------------------------------------
@@ -27,12 +29,12 @@ oneTimeSetUp() {
   export TERM="xterm-256color"
 
   source "$SPACESHIP_ROOT/spaceship.zsh"
-  source "$(dirname $CWD)/spaceship-section.plugin.zsh"
+  source "$(dirname $CWD)/spaceship-vue.plugin.zsh"
 
   SPACESHIP_PROMPT_ASYNC=false
   SPACESHIP_PROMPT_FIRST_PREFIX_SHOW=true
   SPACESHIP_PROMPT_ADD_NEWLINE=false
-  SPACESHIP_PROMPT_ORDER=(foobar)
+  SPACESHIP_PROMPT_ORDER=(vue)
 
   echo "Spaceship version: $(spaceship --version)"
 }
@@ -48,7 +50,7 @@ oneTimeTearDown() {
 # TEST CASES
 # ------------------------------------------------------------------------------
 
-test_incorrect_env() {
+test_no_package() {
   local expected=""
   local actual="$(spaceship::testkit::render_prompt)"
 
@@ -56,12 +58,17 @@ test_incorrect_env() {
 }
 
 test_mocked_version() {
-  # Prepare the environment
-  touch $SHUNIT_TMPDIR/test.foo
+  setupVue
 
-  local prefix="%{%B%}$SPACESHIP_FOOBAR_PREFIX%{%b%}"
-  local content="%{%B%F{$SPACESHIP_FOOBAR_COLOR}%}$SPACESHIP_FOOBAR_SYMBOL$mocked_version%{%b%f%}"
-  local suffix="%{%B%}$SPACESHIP_FOOBAR_SUFFIX%{%b%}"
+  local mocked_version="16.0.0-mocked"
+  # FIXME: Remove this mock when updated docker image is released
+  spaceship::datafile() {
+    echo $mocked_version
+  }
+
+  local prefix="%{%B%}$SPACESHIP_VUE_PREFIX%{%b%}"
+  local content="%{%B%F{$SPACESHIP_VUE_COLOR}%}${SPACESHIP_VUE_SYMBOL}v$mocked_version%{%b%f%}"
+  local suffix="%{%B%}$SPACESHIP_VUE_SUFFIX%{%b%}"
 
   local expected="$prefix$content$suffix"
   local actual="$(spaceship::testkit::render_prompt)"
